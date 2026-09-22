1b24d5ecb179026a90fee5b46640c0ca048a817d
Farhad Aliyev <farhad@kanni.pro>
2026-09-22 15:42:20 +0400

    feat: gas treasary module

--- src/components/PhotoWidget3D.tsx
+++ src/components/PhotoWidget3D.tsx
@@ -3,6 +3,8 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import styles from "./PhotoWidget3D.module.css";
 
+
+
 const IMAGE_DURATION_MS = 8000;
 const FADE_MS = 800;
 const POLL_INTERVAL_MS = 30_000;
